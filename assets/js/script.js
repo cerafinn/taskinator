647c2501c5151e5180f07ac0269cc8a268e05427
cerafinn <74612014+cerafinn@users.noreply.github.com>
2021-01-13 01:19:39 -0500

    added ability to submit custom tasks, added reset after task is added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,28 @@
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function() {
+var taskFormHandler = function() {
   event.preventDefault();
-
   //get information for task
   var taskNameInput = document.querySelector("input[name='task-name']").value;
   var taskTypeInput = document.querySelector("select[name='task-type']").value;
+  //confirm there is input
+  if (!taskNameInput || !taskTypeInput) {
+    alert("You need to fill out the task form!");
+    return false;
+  }
+  //reset
+  formEl.reset();
+  //package data as an object
+  var taskDataObj = {
+    name: taskNameInput,
+    type: taskTypeInput,
+  };
+  //send to next function to format
+  createTaskEl(taskDataObj);
+};
 
+var createTaskEl = function(taskDataObj) {
   //create list item
   var listItemEl = document.createElement("li");
   listItemEl.className = "task-item";
@@ -15,10 +30,11 @@ var createTaskHandler = function() {
   var taskInfoEl = document.createElement("div");
   taskInfoEl.className = "task-info";
   //add html content to div
-  taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+  taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
   listItemEl.appendChild(taskInfoEl);
   //add entire list item to list
   tasksToDoEl.appendChild(listItemEl);
-}
 
-formEl.addEventListener("submit", createTaskHandler);
+};
+
+formEl.addEventListener("submit", taskFormHandler);
