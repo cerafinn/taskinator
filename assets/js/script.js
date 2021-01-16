28d64b7e61cf7af7df3851834bf9b074b032eb35
cerafinn <74612014+cerafinn@users.noreply.github.com>
2021-01-16 18:05:40 -0500

    added drag and drop options

--- assets/js/script.js
+++ assets/js/script.js
@@ -46,6 +46,7 @@ var createTaskEl = function(taskDataObj) {
 
   //add task id
   listItemEl.setAttribute("data-task-id", taskIdCounter);
+  listItemEl.setAttribute("draggable", "true");
 
   //create div to hold task info and add to list item
   var taskInfoEl = document.createElement("div");
@@ -103,8 +104,6 @@ var createTaskActions = function(taskId) {
 
   return actionContainerEl;
 };
-
-formEl.addEventListener("submit", taskFormHandler);
 
 var deleteTask = function(taskId) {
   var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
@@ -173,6 +172,58 @@ var taskStatusChangeHandler = function(event) {
   }
 };
 
+var dragTaskHandler = function(event) {
+  var taskId = event.target.getAttribute("data-task-id");
+  event.dataTransfer.setData("text/plain", taskId)
+  var getId = event.dataTransfer.getData("text/plain");
+};
+
+var dropZoneDragHandler = function(event) {
+  var taskListEl = event.target.closest(".task-list");
+  if (taskListEl) {
+  event.preventDefault();
+  taskListEl.setAttribute("style", "background: rbga(68, 233, 255, 0.7); border-style: dashed;");
+  };
+};
+
+var dropTaskHandler = function(event) {
+  var id = event.dataTransfer.getData("text/plain");
+  var draggableElement = document.querySelector("[data-task-id='" + id + "']");
+  var dropZoneEl = event.target.closest(".task-list");
+  var statusType = dropZoneEl.id;
+  //set status of task based on dropZone id
+  var statusSelectEl = draggableElement.querySelector("select[name='status-change']");
+  if (statusType === "tasks-to-do") {
+    statusSelectEl.selectedIndex = 0;
+  }
+  else if (statusType === "tasks-in-progress") {
+    statusSelectEl.selectedIndex = 1;
+  }
+  else if (statusType === "tasks-completed") {
+    statusSelectEl.selectedIndex = 2;
+  }
+
+  dropZoneEl.removeAttribute("style");
+  dropZoneEl.appendChild(draggableElement);
+};
+
+var dragLeaveHandler = function(event) {
+  var taskListEl = event.target.closest(".task-list");
+  if (taskListEl) {
+  taskListEl.removeAttribute("style");
+  }
+};
+
 pageContentEl.addEventListener("click", taskButtonHandler);
 
 pageContentEl.addEventListener("change", taskStatusChangeHandler);
+
+formEl.addEventListener("submit", taskFormHandler);
+
+pageContentEl.addEventListener("dragstart", dragTaskHandler);
+
+pageContentEl.addEventListener("dragover", dropZoneDragHandler);
+
+pageContentEl.addEventListener("drop", dropTaskHandler);
+
+pageContentEl.addEventListener("dragleave", dragLeaveHandler);
